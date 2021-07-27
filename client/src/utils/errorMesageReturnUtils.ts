336b7b465500b96717786b01bf7ba1c026065503
Kyle <kylenguyen1014@github.com>
2021-07-26 23:34:57 -0400

    create a function take in error from server and return its message

---
+++ client/src/utils/errorMesageReturnUtils.ts
@@ -0,0 +1,14 @@
+import { AxiosError } from "axios"
+
+export const errorMessageReturn = (error : AxiosError) => {
+    if (error.request === undefined || error.response === undefined) {
+        return 'Network Error'
+    }
+
+    if (error.response && error.response.data) {
+        return JSON.stringify(error.response.data).slice(0, 100) + '...'
+    }
+
+    return error.message
+}
+
