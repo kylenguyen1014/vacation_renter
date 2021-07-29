054c05cfb44db84148fb283d5170011ca96db14b
Kyle Nguyen <knguyen@kendausa.com>
2021-07-29 16:55:17 -0400

    create a loading backdrop for showing loading state between api hits

---
+++ client/src/components/LoadingBackDrop/LoadingBackDrop.tsx
@@ -0,0 +1,18 @@
+import { Backdrop, LinearProgress } from '@material-ui/core'
+import React, { ReactElement } from 'react'
+import { useIsFetching } from 'react-query'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../redux/root-reducer'
+
+function LoadingBackDrop(): ReactElement {
+    const fetching = useSelector((state: RootState) => state.fetching)
+    const isFetching = useIsFetching()
+
+    return (
+        <Backdrop open={fetching.isFetching || isFetching > 0} className='App-backdrop' style={{ zIndex: 1300 }}>
+            <LinearProgress color='secondary' />
+        </Backdrop>
+    )
+}
+
+export default LoadingBackDrop
