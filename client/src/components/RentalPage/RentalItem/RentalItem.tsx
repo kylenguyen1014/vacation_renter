976c28fce0970aa0b4f8a260e97532eb722a2e7b
Kyle Nguyen <knguyen@kendausa.com>
2021-07-29 16:55:54 -0400

    create a RentalItem component

---
+++ client/src/components/RentalPage/RentalItem/RentalItem.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from 'react'
+import { Rental } from '../../../shared/interfaces/Rental'
+
+interface Props {
+    rental : Rental
+}
+
+function RentalItem({rental}: Props): ReactElement {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RentalItem
